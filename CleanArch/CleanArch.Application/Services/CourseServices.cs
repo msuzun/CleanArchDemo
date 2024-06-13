d00edfc51ffc3b11e80dffa522383a1349f74775
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-14 01:53:33 +0300

    Service implementation

---
+++ CleanArch/CleanArch.Application/Services/CourseServices.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseServices : ICourseService
+    {
+        private ICourseRepository _courseRepository;
+        public CourseServices(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+        public IEnumerable<CourseViewModel> GetCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
