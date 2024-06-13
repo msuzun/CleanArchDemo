27ab135f079fedb590868855c1426c8185262d73
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-14 02:29:00 +0300

    adding images and coursecontroller

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
