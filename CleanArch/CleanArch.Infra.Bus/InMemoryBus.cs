e4c8c187426b2c3e148fd7b43c1346b51431d7d3
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-15 01:13:41 +0300

    Infrastructure Bus implementation

--- CleanArch/CleanArch.Infra.Bus/InMemoryBus.cs
+++ CleanArch/CleanArch.Infra.Bus/InMemoryBus.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CleanArch.Infra.Bus
 {
-    public class InMemoryBus
+    public sealed class InMemoryBus : IMediatorHandler
     {
+        private readonly IMediator _mediator;
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public Task SendCommand<T>(T command) where T: Command
+        {
+            return _mediator.Send(command);
+        }
     }
 }
