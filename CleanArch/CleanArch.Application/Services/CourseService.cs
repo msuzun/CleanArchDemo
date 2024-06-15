8b790a2b9e9d1713ed28534eb77ba83cd7dbfbbd
Muhammet Şevki UZUN <sevki_uzun53@hotmail.com>
2024-06-15 12:50:56 +0300

    Refactor Course Service Get method

--- CleanArch/CleanArch.Application/Services/CourseService.cs
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
 using CleanArch.Domain.Commands;
@@ -27,12 +28,9 @@ namespace CleanArch.Application.Services
             _bus.SendCommand(_automapper.Map<CreateCourseCommand>(courseViewModel));
         }
 
-        public CourseViewModel GetCourses()
+        public IEnumerable<CourseViewModel> GetCourses()
         {
-            return new CourseViewModel()
-            {
-                Courses = _courseRepository.GetCourses()
-            };
+            return _courseRepository.GetCourses().ProjectTo<CourseViewModel>(_automapper.ConfigurationProvider);
         }
     }
 }
